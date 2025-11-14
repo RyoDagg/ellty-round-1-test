757fad7faf8750fd2ee9dd03983bfe4bc2f7fbdc
RyoDagg <mannai.abdlkader@gmail.com>
2025-11-14 21:55:01 +0100

    adjust button height

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 const Button = ({ children }: { children: ReactNode }) => {
   return (
-    <button className="bg-[#FFCE22] rounded-sm px-2 py-4 cursor-pointer active:cursor-default w-full hover:bg-[#FFD84D] active:bg-[#FFCE22]">
+    <button className="bg-[#FFCE22] rounded-sm h-10 px-2 cursor-pointer active:cursor-default w-full hover:bg-[#FFD84D] active:bg-[#FFCE22]">
       {children}
     </button>
   );
