cef928d85b2bd78ccaf119717d2bb35061385d59
RyoDagg <mannai.abdlkader@gmail.com>
2025-11-14 22:28:14 +0100

    Adjust and streamline Checkbox Component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,46 +1,35 @@
-import { useState } from 'react';
 import { HiCheck } from 'react-icons/hi';
 
 export default function Checkbox({
   checked,
   onChange,
-  ...rest
 }: {
   checked?: boolean;
-  onChange?: (value: boolean) => void;
-  [key: string]: any;
+  onChange: () => void;
 }) {
-  const [isChecked, setIsChecked] = useState(checked ?? false);
-
-  const toggle = () => {
-    const next = !isChecked;
-    setIsChecked(next);
-    onChange?.(next);
-  };
-
   return (
     <button
       type="button"
-      onClick={toggle}
+      onClick={onChange}
       className={`
-        h-6 w-6 flex items-center justify-center cursor-pointer rounded-md transition-all group border m-4
+        h-6 w-6 flex items-center justify-center cursor-pointer rounded-md transition-all group border
         active:shadow-[0_0_0_0.2rem_rgba(36,105,246,0.1)]
         ${
-          isChecked
+          checked
             ? 'border-[#2469F6] bg-[#2469F6] hover:border-[#5087F8] hover:bg-[#5087F8] active:bg-[#2469F6] active:border-[#2469F6]'
             : 'border-[#CDCDCD] bg-white hover:border-[#BDBDBD]'
         }
       `}
-      {...rest}
     >
       <HiCheck
         className={`
-            h-4 w-4 
-            ${
-              isChecked
-                ? 'text-white'
-                : 'text-transparent group-hover:text-[#E3E3E3] group-active:text-[#878787]'
-            }`}
+          h-6 w-6
+          ${
+            checked
+              ? 'text-white'
+              : 'text-transparent group-hover:text-[#E3E3E3] group-active:text-[#878787]'
+          }
+        `}
       />
     </button>
   );
