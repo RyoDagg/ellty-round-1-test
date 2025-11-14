fc7cc683c7d5d93563acdccc5e55ebf23c1ae70a
RyoDagg <mannai.abdlkader@gmail.com>
2025-11-14 22:28:27 +0100

    Create PageRow Component

---
+++ src/components/PageRow.tsx
@@ -0,0 +1,21 @@
+import Checkbox from './Checkbox';
+
+export default function PageRow({
+  title,
+  selected,
+  onChange,
+}: {
+  title: string;
+  selected: boolean;
+  onChange: () => void;
+}) {
+  return (
+    <div
+      onClick={onChange}
+      className="flex items-center justify-between bg-white rounded-md py-2 pl-5 pr-3 cursor-pointer"
+    >
+      <span className="text-gray-700 select-none">{title}</span>
+      <Checkbox checked={selected} onChange={onChange} />
+    </div>
+  );
+}
